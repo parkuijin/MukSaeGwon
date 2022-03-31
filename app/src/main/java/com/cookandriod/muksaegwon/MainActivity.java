df42a3935b9fe5dec37aed3fbd8306cf800a00e7
uijin <uijin29@naver.com>
2022-03-31 12:29:43 +0900

    gradle, manifest, java 수정

--- app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
@@ -2,18 +2,10 @@ package com.cookandriod.muksaegwon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.MapView;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         MapView mapView = new MapView(this);
 
-        //지도 띄우기
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.mapView);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        //지도 띄우기
         mapViewContainer.addView(mapView);
 
     }
