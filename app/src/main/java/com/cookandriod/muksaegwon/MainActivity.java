c751dbe0547911a4e2fdca73bfaa6061be1c0078
uijin <uijin29@naver.com>
2022-03-31 16:30:07 +0900

    Fragment 추가 및 ActionBar 제거

--- app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
@@ -1,19 +1,39 @@
 package com.cookandriod.muksaegwon;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ConstraintLayout Container;
+    MapFragment MapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // HASH METHOD DELETED
+
+        //ActionBar hide
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.Container, MapFragment);
+        fragmentTransaction.commit();
 
         MapView mapView = new MapView(this);
 
