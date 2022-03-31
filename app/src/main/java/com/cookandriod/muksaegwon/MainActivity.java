603bb2f4ae5bc09b1b8a0da5b04a12ea69219442
uijin <uijin29@naver.com>
2022-03-31 12:02:35 +0900

    Manifest에 지도 코드 추가 XML에 지도 추가

--- app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
@@ -8,6 +8,9 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.MapView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -18,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // HASH METHOD DELETED
+
+        MapView mapView = new MapView(this);
+
+        //지도 띄우기
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
     }
 
 };
