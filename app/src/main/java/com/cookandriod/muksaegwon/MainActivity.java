f7d0d1c59cd0e410c1c4e0ad4939f382ac43622b
hyunseung <hxxxsxxxx98@gmail.com>
2022-03-31 11:42:33 +0900

    해쉬 메소드 생성(추가)

--- app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandriod/muksaegwon/MainActivity.java
@@ -18,8 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAppKeyHash();
+
 
         // HASH METHOD DELETED
     }
-
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key: ", something);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("name not found", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    };
 };
