c86e5e306bb1637a4493124c9e5c2c935cf03e28
uijin <uijin29@naver.com>
2022-03-31 16:30:07 +0900

    Fragment 추가 및 ActionBar 제거

---
+++ app/src/main/java/com/cookandriod/muksaegwon/MapFragment.java
@@ -0,0 +1,20 @@
+package com.cookandriod.muksaegwon;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return v;
+    }
+}
