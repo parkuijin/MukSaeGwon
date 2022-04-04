8472f6b6046be33788eb40330ef031af80338ef5
uijin <uijin29@naver.com>
2022-04-04 21:24:00 +0900

    코드 수정 중간 저장

--- app/src/main/java/com/cookandriod/muksaegwon/RegisterActivity.java
+++ app/src/main/java/com/cookandriod/muksaegwon/RegisterActivity.java
@@ -1,11 +1,27 @@
 package com.cookandriod.muksaegwon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class RegisterActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = MapFragment.class.getSimpleName();
+
+    // GoogleMap
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +32,52 @@ public class RegisterActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(getApplicationContext(), R.raw.style_json));
+
+            if (!success) {
+                Toast.makeText(getApplicationContext(), "Style parsing failed.", Toast.LENGTH_LONG).show();
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions(); // 마커 생성
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울"); // 마커 제목
+        markerOptions.snippet("한국의 수도"); // 마커 설명
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL)); // 초기 위치
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15)); // 줌의 정도
+
+        // 맵 터치 이벤트 구현
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                mMap.clear();
+                MarkerOptions foodTruck = new MarkerOptions();
+                // 마커 타이틀
+                foodTruck.title("마커 좌표");
+                Double latitude = point.latitude; // 위도
+                Double longitude = point.longitude; // 경도
+                // 마커의 스니펫(간단한 텍스트) 설정
+                foodTruck.snippet(latitude.toString() + ", " + longitude.toString());
+                // LatLng: 위도 경도 쌍을 나타냄
+                foodTruck.position(new LatLng(latitude, longitude));
+                // 마커 추가
+                mMap.addMarker(foodTruck);
+            }
+        });
     }
 }
