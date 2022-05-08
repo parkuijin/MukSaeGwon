6f20ec9cf2bed7430084c0223dbd5cadba1ab315
Uijin Park <uijin29@naver.com>
2022-05-08 20:07:24 +0900

    chore : MainActivity.java 간단한 코드 수정
    MapFragment 겹치는 부분 코드 삭제

--- app/src/main/java/com/cookandroid/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/MainActivity.java
@@ -1,33 +1,14 @@
 package com.cookandroid.muksaegwon;
 
-import static java.security.AccessController.getContext;
-
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     // MapFragment
@@ -36,15 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     // Button
     ImageView CategoryButton, RegisterButton, MapButton, MypageButton;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private Button curButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        curButton = (Button)findViewById(R.id.CurButton);
 
         // ActionBar hide
         ActionBar actionBar = getSupportActionBar();
