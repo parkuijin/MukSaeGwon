de3c099c33b223b0b76c00dcfec522e57e833ef9
100soooolah <bsa9811@naver.com>
2022-05-11 11:52:38 +0900

    마이 페이지 버튼 클릭+액티비티 이동까지

--- app/src/main/java/com/cookandroid/muksaegwon/mypageActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/mypageActivity.java
@@ -2,16 +2,57 @@ package com.cookandroid.muksaegwon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.cookandroid.muksaegwon.R;
 
 public class mypageActivity extends AppCompatActivity {
+    ImageView setting,heart,review;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+
+        // 셋팅 액티비티 열기
+        /*
+        setting = (ImageView) findViewById(R.id.settingBtn);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+        */
+
+        //찜 페이지 액티비티 열기
+
+        heart = (ImageView) findViewById(R.id.heartBtn);
+        heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        //리뷰 페이지 액티비티 열기
+        review = (ImageView) findViewById(R.id.reviewBtn);
+        review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
 
