1bc81e10df60db6e96656ce69eb4cf921661c868
hyunseung <hxxxsxxxx98@gmail.com>
2022-05-11 11:35:41 +0900

    가게정보등록 액티비티 이름 수정

--- app/src/main/java/com/cookandroid/muksaegwon/InfoRegisterActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/InfoRegisterActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FoodTruckInfoActivity extends AppCompatActivity {
+public class InfoRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_truck_info);
+
     }
 }
