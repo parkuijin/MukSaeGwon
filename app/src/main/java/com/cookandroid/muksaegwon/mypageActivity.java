4828650214148e5eb21d9b7f01a6187a792adc8a
leejuhyun <smilejh0423@naver.com>
2022-05-11 10:49:59 +0900

    gaaa
    aaa

---
+++ app/src/main/java/com/cookandroid/muksaegwon/mypageActivity.java
@@ -0,0 +1,19 @@
+package com.cookandroid.muksaegwon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cookandroid.muksaegwon.R;
+
+public class mypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+
+
+
+    }
+}
