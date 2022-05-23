0544212697581379ade8716bdc48eba857e8e8ec
Uijin Park <uijin29@naver.com>
2022-05-24 06:45:20 +0900

    feat : LayoutInflater를 이용한 메뉴 추가 기능
    메뉴 추가 기능만 구현
    삭제 기능과 값 DB 저장 관련은 구현 예정

--- app/src/main/java/com/cookandroid/muksaegwon/InfoRegisterActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/InfoRegisterActivity.java
@@ -1,21 +1,28 @@
 package com.cookandroid.muksaegwon;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class InfoRegisterActivity extends AppCompatActivity {
 
-    ImageView infoRegFinBtn;
+    ImageView infoRegFinBtn, plusMenu, minusMenu;
     CheckBox cash, creditCard, account, mon, tue, wed, thu, fri, sat, sun;
     EditText storeName;
     TextView storeLocation;
+
+    LinearLayout menuContainer;
+    ConstraintLayout a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,10 @@ public class InfoRegisterActivity extends AppCompatActivity {
         // ActionBar hide
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        plusMenu = (ImageView) findViewById(R.id.plusMenuBtn);
+        minusMenu = (ImageView) findViewById(R.id.minusMenuBtn);
+        menuContainer = (LinearLayout) findViewById(R.id.menuItemLayout);
 
         infoRegFinBtn = (ImageView) findViewById(R.id.regInfoFinBtn);
         cash = (CheckBox) findViewById(R.id.checkCash);
@@ -42,6 +53,30 @@ public class InfoRegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        plusMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tvMenuName, tvMenuPrice;
+                EditText etMenuName, etMenuPrice;
+
+                // LayoutInflater inflater = LayoutInflater.from(MenuActivity.this);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                inflater.inflate(R.layout.item_menu, menuContainer, true);
+                tvMenuName = menuContainer.findViewById(R.id.tvMenuName);
+                etMenuName = menuContainer.findViewById(R.id.etMenuName);
+                tvMenuPrice = menuContainer.findViewById(R.id.tvMenuPrice);
+                etMenuPrice = menuContainer.findViewById(R.id.etMenuPrice);
+            }
+        });
+
+        minusMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
             }
         });
 
