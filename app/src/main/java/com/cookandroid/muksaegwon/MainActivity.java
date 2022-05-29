c1063af7bb85c398fdc58e9bdfd9b6564df7b7c5
Uijin Park <uijin29@naver.com>
2022-05-29 11:34:00 +0900

    test : Parsing Test

--- app/src/main/java/com/cookandroid/muksaegwon/MainActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/MainActivity.java
@@ -3,6 +3,7 @@ package com.cookandroid.muksaegwon;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,6 +11,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.cookandroid.muksaegwon.controller.MsgXmlParser;
+import com.cookandroid.muksaegwon.model.Category;
+import com.cookandroid.muksaegwon.model.Menu;
+import com.cookandroid.muksaegwon.model.PayWay;
+import com.cookandroid.muksaegwon.model.Store;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     // MapFragment
@@ -36,13 +45,52 @@ public class MainActivity extends AppCompatActivity {
                 ActionBar actionBar = getSupportActionBar();
                 actionBar.hide();
 
-                mapFragment = new
 
-                        MapFragment();
+                // 이 아래로 테스트 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                String text = "<storeName>abc</storeName>" +
+                        "<lat>37.122</lat> " +
+                        "<lng>122.436</lng>" +
+                        " <menu>[{\"name\":\"aaa\",\"price\":\"1000\"},{\"name\":\"bbb\",\"price\":\"3000\"}]</menu> " +
+                        "<payWay>{\"cash\": true,\"card\": false,\"account\": true}</payWay> " +
+                        "<isRunning>0</isRunning> " +
+                        "<runDay>aaa</runDay> " +
+                        "<openTime>9</openTime> " +
+                        "<offTime>9</offTime>" +
+                        "<category>{\n" +
+                        "\t\"corn\": true,\n" +
+                        "\t\"fish\": false,\n" +
+                        "\t\"topokki\": true,\n" +
+                        "\t\"eomuk\": true,\n" +
+                        "\t\"sweetpotato\": true,\n" +
+                        "\t\"toast\": true,\n" +
+                        "\t\"takoyaki\": true,\n" +
+                        "\t\"waffle\": true,\n" +
+                        "\t\"dakggochi\": true\n" +
+                        "}</category>";
 
-                fragmentManager =
+                ArrayList<Category> categories = new ArrayList<>();
+                ArrayList<PayWay> payWays = new ArrayList<>();
+                ArrayList<Store> stores = new ArrayList<>();
+                ArrayList<Menu> menus = new ArrayList<>();
 
-                        getSupportFragmentManager();
+                MsgXmlParser msgXmlParser = new MsgXmlParser(text);
+                msgXmlParser.xmlParsingSFM(stores);
+
+                Log.i("test",stores.get(0).getMenus().toString());
+
+                msgXmlParser.jsonParsingMNP(stores.get(0).getMenus(), menus);
+                msgXmlParser.jsonParsingPW(stores.get(0).getPayWay(), payWays);
+                msgXmlParser.jsonParsingCTG(stores.get(0).getCategory(), categories);
+                String a = categories.get(0).getCorn(); // corn의 값이 반환
+
+                // 이 위로 테스트 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+                mapFragment = new MapFragment();
+
+                fragmentManager = getSupportFragmentManager();
 
                 // Google Map 프래그먼트 출력
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
