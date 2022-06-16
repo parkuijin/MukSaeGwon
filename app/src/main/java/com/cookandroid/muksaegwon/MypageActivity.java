cf04b4f7cbb5484239ac5a7529720171008be9e8
hyunseung <hxxxsxxxx98@gmail.com>
2022-06-17 00:16:27 +0900

    프로필 이미지 수정

--- app/src/main/java/com/cookandroid/muksaegwon/MypageActivity.java
+++ app/src/main/java/com/cookandroid/muksaegwon/MypageActivity.java
@@ -2,7 +2,11 @@ package com.cookandroid.muksaegwon;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,13 +15,19 @@ import android.widget.TextView;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MypageActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 1000;
 
-    ImageView backBtn;
+    ImageView backBtn, profile;
     TextView nameTv, emailTv;
     LinearLayout heart,review;
     private GoogleSignInClient mGoogleSignInClient;
@@ -38,10 +48,16 @@ public class MypageActivity extends AppCompatActivity {
         nameTv = (TextView)findViewById(R.id.nameTv);
         review = (LinearLayout) findViewById(R.id.reviewBtn);
         heart = (LinearLayout) findViewById(R.id.heartBtn);
+        profile = (ImageView)findViewById(R.id.profile);
 
         preferences = getApplicationContext().getSharedPreferences("userInfo",MODE_PRIVATE);
         String name = preferences.getString("name","");
         String email = preferences.getString("email","");
+
+        new DownloadImageTask(profile)
+                .execute("https://lh3.googleusercontent.com/a-/AOh14Gg_QB13iJ8aYHge5SjaCTd9Jg3csmTcdDRf7DcX=s576-p-no");
+
+
         nameTv.setText(name);
         emailTv.setText(email);
 
@@ -72,4 +88,31 @@ public class MypageActivity extends AppCompatActivity {
             }
         });
     }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            Glide.with(getApplicationContext()).load(result).circleCrop().into(bmImage);
+//            bmImage.setImageBitmap(result);
+        }
+    }
+
 }
